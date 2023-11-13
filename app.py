ffe36dcc900f44400c4775f79fbada33e45298aa
danielge149 <danielgetial149@gmail.com>
2023-11-13 15:34:16 -0500

    Version 4

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask import redirect
 from flask import url_for
 from flask import render_template
 from os import listdir
-from forms import FormCalculadora
-from forms import UploadForm
 from werkzeug.utils import secure_filename
 import os
 
@@ -24,7 +22,7 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 DB_HOST = "localhost"
 DB_NAME = "seminario"
 DB_USER = "postgres"
-DB_PASS = "12345"
+DB_PASS = "123"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
@@ -40,9 +38,7 @@ def inicio():
     
         # User is loggedin show them the home page
         #return render_template('home.html', username=session['username'])
-        template_base = render_template("base.html")
-        template_inicio = render_template("inicio.html")
-        return template_base + template_inicio
+        return render_template("inicio.html")
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
     
@@ -200,137 +196,14 @@ def restaurantes():
 def not_found_error(error):
     return render_template('error.html', error_code=404), 404
 
+# rutas restaurantes 
+@app.route('/restaurantes/pizzeria')
+def pizzeria():
+    return render_template('pizzeria.html')
 
-@app.route('/hola/')
-@app.route('/hola/<nombre>')
-def hola(nombre=None):
-    return render_template("template1.html", nombre=nombre)
-
-
-# @app.route('/adicion/<num1>/<num2>')
-# def adicion(num1, num2):
-#     try:
-#         resultado = int(num1) + int(num2)
-#     except ValueError:
-#         abort(404)
-#         # abort(404, "Página NO ENCONTRADA")
-#     return render_template("template2.html", num1=num1, num2=num2,
-#                            resultado=resultado)
-
-
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return (render_template("error.html", error="Página no encontrada..."),
-#             404)
-#     # return render_template("error.html", error=error.description), 404
-
-
-# @app.route('/tabla/<numero>')
-# def tabla(numero):
-#     try:
-#         numero = int(numero)
-#     except ValueError:
-#         abort(404)
-#     return render_template("template3.html", num=numero)
-
-
-# @app.route('/enlaces')
-# def enlaces():
-#     enlaces = [{"url": "http://www.google.es", "texto": "Google"},
-#                {"url": "http://www.twitter.com", "texto": "Twitter"},
-#                {"url": "http://www.facebook.com", "texto": "Facebook"}, ]
-#     return render_template("template4.html", enlaces=enlaces)
-
-
-# -------- Trabajando con Formularios ---------- #
-
-# @app.route("/calculadora_post", methods=["GET", "POST"])
-# def calculadora_post():
-#     if request.method == "POST":
-#         num1 = request.form.get("num1")
-#         num2 = request.form.get("num2")
-#         operador = request.form.get("operador")
-#         try:
-#             resultado = eval(num1 + operador + num2)
-#         except Exception:
-#             return render_template("error.html",
-#                                    error="No puedo realizar la operación")
-#         return render_template("resultado.html", num1=num1, num2=num2,
-#                                operador=operador, resultado=resultado)
-#     else:
-#         return render_template("calculadora_post.html")
-
-
-# @app.route("/calculadora_get", methods=["GET"])
-# def calculadora_get():
-#     if request.method == "GET" and len(request.args) > 0:
-#         num1 = request.args.get("num1")
-#         num2 = request.args.get("num2")
-#         operador = request.args.get("operador")
-#         try:
-#             resultado = eval(num1 + operador + num2)
-#         except Exception:
-#             return render_template("error.html",
-#                                    error="No puedo realizar la operación")
-#         return render_template("resultado.html", num1=num1, num2=num2,
-#                                operador=operador, resultado=resultado)
-#     else:
-#         return render_template("calculadora_get.html")
-
-
-# @app.route("/calculadora/<operador>/<num1>/<num2>", methods=["GET"])
-# def calculadora_var(operador, num1, num2):
-#     try:
-#         resultado = eval(num1 + operador + num2)
-#     except Exception:
-#         return render_template("error.html",
-#                                error="No puedo realizar la operación")
-#     return render_template("resultado.html", num1=num1, num2=num2,
-#                            operador=operador, resultado=resultado)
-
-
-# -------- Generando formularios con flask-wtf ---------- #
-
-# @app.route("/calculadora_wtf", methods=["GET", "POST"])
-# def calculadora_wtf():
-#     form = FormCalculadora(request.form)
-#     if form.validate_on_submit():
-#         num1 = form.num1.data
-#         num2 = form.num2.data
-#         operador = form.operador.data
-#         try:
-#             resultado = eval(str(num1) + operador + str(num2))
-#             print(resultado)
-#         except Exception:
-#             return render_template("error.html",
-#                                    error="No puedo realizar la operación")
-#         return render_template("resultado.html", num1=num1, num2=num2,
-#                                operador=operador, resultado=resultado)
-#     else:
-#         return render_template("calculadora_wtf.jinja2", form=form)
-
-
-# -------- Subida de Archivos ---------- #
-
-# @app.route('/archivos')
-# def archivos():
-#     lista = []
-#     for file in listdir(app.root_path+"/static/img"):
-#         lista.append(file)
-#     return render_template("archivos.html", lista=lista)
-
-
-# @app.route('/subir_imagen', methods=['GET', 'POST'])
-# def upload():
-#     form = UploadForm()  # carga request.form y request.file
-#     if form.validate_on_submit():
-#         f = form.photo.data
-#         filename = secure_filename(f.filename)
-#         f.save(app.root_path + "/static/img/" + filename)
-#         return redirect(url_for('archivos'))
-#     return render_template('upload_image.html', form=form)
-
-
+@app.route('/restaurantes/sushi')
+def sushi():
+    return render_template('sushi.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
